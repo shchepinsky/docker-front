224a1ec843f1eac6b89ecf384b87f84d9a4bf055
shchepinsky <dima.shchepinsky@gmail.com>
2020-11-16 00:57:06 +0200

    changed app text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test
+          changed from pull request
         </p>
       </header>
     </div>

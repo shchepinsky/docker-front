e965bf288015e13f2977728391bfd7281bc3f3de
shchepinsky <dima.shchepinsky@gmail.com>
2020-11-19 22:24:57 +0200

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           changed from pull request
         </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
